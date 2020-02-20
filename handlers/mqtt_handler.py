4b26a5e9e5bd17f577b3e9de80a78d6451a83446
Eoin Coogan <38385465+CoogyEoin@users.noreply.github.com>
2020-02-20 12:19:52 +0000

    Adding docstrings to mqtt_handler

--- handlers/mqtt_handler.py
+++ handlers/mqtt_handler.py
@@ -3,6 +3,16 @@ import paho.mqtt.client as mqtt
 class MQTTHandler:
 
 	def __init__(self, mqtt_server, mqtt_uplink_topic, mqtt_downlink_topic):
+		"""
+        	MQTTHandler class
+        	This class is used for connecting to an MQTT server and -
+		handling the uplink and downlink messages from the server.
+      
+        	---Arguments---
+        	mqtt_server: The server the handler is to connect to
+        	mqtt_uplink_topic: The topic the handler will publish messages to
+        	mqtt_downlink_topic: The topic the handler will subscribe to.
+        	"""
 		self.the_mqtt_server = mqtt_server
 		self.the_mqtt_uplink_topic = mqtt_uplink_topic
 		self.the_mqtt_downlink_topic = mqtt_downlink_topic
@@ -10,12 +20,22 @@ class MQTTHandler:
 		self.mqtt_setup()
 
 	def mqtt_setup(self):
+		"""
+		Method used to connect to MQTT server and set the -
+		callbacks for message arrival/connection.
+		"""
 		self.client.on_connect = self.connect_callback()
 		self.client.on_message = on_message
 		self.client.connect(self.the_mqtt_server, 1883, 60)
 
 
 	def publish_mqtt_message(self, message):
+		"""
+		Method for publishing messages to the MQTT uplink topic.
+		
+		---Arguments---
+		message: The message being sent to the MQTT topic
+		"""
 		self.client.publish(self.the_mqtt_uplink_topic, message)
 		print("Publishing MQTT message: " + message)
 
